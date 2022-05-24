d9e816aa40ffa9bd60c634af37dd2e99d51bf096
Nigel <nigelsmyth@hotmail.com>
2022-05-24 16:42:35 +0100

    Test commit

--- AWSLambdaToDynamoDB/Function.cs
+++ AWSLambdaToDynamoDB/Function.cs
@@ -55,6 +55,7 @@ namespace AWSLambdaToDynamoDB
                 });
         }
 
+        // 
         //Function Handler is an entry point to start execution of Lambda Function.
         //It takes Input Data as First Parameter and ObjectContext as Second
         public async Task FunctionHandler(Customer customer, ILambdaContext context)
